b233d8b5325e523b13f0c1dbb4796d7b30290e63
albert-kepner <albert.kepner@outlook.com>
2026-04-05 17:25:24 -0400

    apr04_mr_wrong

--- mr_wrong_number_2/src/main.rs
+++ mr_wrong_number_2/src/main.rs
@@ -302,7 +302,7 @@ impl Trial  {
             for assignment in &assignments {
                 println!("Assignment for person_index {}: possible_positions: {:?} position = {:?}", assignment.person_index, assignment.possible_positions, assignment.position);
             }
-            consistent = is_consistent(&assignments, self.num_people);
+            consistent = !other_lies && is_consistent(&assignments, self.num_people);
        
         (other_lies, liar_lies, consistent)
     }
